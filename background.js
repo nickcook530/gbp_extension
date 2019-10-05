bca4d7898d66c89760aa60c31f1876e8cc35d784
Nicholas Cook <nickcook530@gmail.com>
2019-10-05 18:40:10 -0400

    Changed results to a list and added initial storage data check in preparation for word synonym API

--- background.js
+++ background.js
@@ -1,9 +1,36 @@
+chrome.runtime.onInstalled.addListener(function(details){
+    if(details.reason == "install"){
+        //call a function to handle a first install
+    }else if(details.reason == "update"){
+        //call a function to handle an update
+    }
+});
+
+chrome.runtime.onStartup.addListener(function(){
+    //Might be able to replace this with onInstalled call
+    console.log("this is the startup call from background.js");
+
+    chrome.storage.local.get(function(result){
+        if (typeof result.word_synonyms === 'undefined') {
+            console.log("NO stored results");
+            //Add code here to pull down word synonyms from API
+        }else{
+            console.log("There are stored results");
+            console.log(result);
+        }
+    });
+});
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.msg === "content_completed") {
             //  To do something
             let category = request.data.search_term;
             console.log(category);
+
+
+
+
             var xhr = new XMLHttpRequest();
             xhr.responseType = 'json';
             xhr.open('GET', "http://givebackpact.herokuapp.com/api/categories/" + category + "/companies");
@@ -12,11 +39,15 @@ chrome.runtime.onMessage.addListener(
                     var result_array = xhr.response;
                     console.log(result_array);
 
-                    chrome.storage.local.set({api_result: result_array}, function() {
+                    chrome.storage.local.set({company_api_result: result_array}, function() {
                         console.log("Storage by background.js complete");
                     });
-                    chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
-                    chrome.browserAction.setBadgeText({text: result_array.length.toString()});
+                    if (result_array.length > 0) {
+                        chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
+                        chrome.browserAction.setBadgeText({text: result_array.length.toString()});
+                    }else {
+                        chrome.browserAction.setBadgeText({text: ''});
+                    };
                 }
                 else {
                     //alert('Request failed.  Returned status of ' + xhr.status);
--- popup.js
+++ popup.js
@@ -2,21 +2,22 @@ window.onload = function() {
     console.log("window.onload running");
 
     let companies;
-    chrome.storage.local.get(['api_result'], function(result) {
-        companies = result.api_result;
+    chrome.storage.local.get(['company_api_result'], function(result) {
+        companies = result.company_api_result;
         console.log("popup.js retrieved data:")
         console.log(companies);
         
-        let parent = document.getElementById("companies");
+        let parent = document.getElementById("company_list");
         companies.forEach(company => {
-            let company_div = document.createElement("div");
+            //let company_div = document.createElement("div");
+            let company_li = document.createElement("li");
             let hyperlink = document.createElement("a");
             let node = document.createTextNode(company.name);
             hyperlink.appendChild(node);
             hyperlink.href = company.website_url
             hyperlink.setAttribute('target', '_blank');
-            company_div.appendChild(hyperlink);
-            parent.appendChild(company_div);
+            company_li.appendChild(hyperlink);
+            parent.appendChild(company_li);
         });
     });
 };
