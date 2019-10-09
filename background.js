05cb09e4f483fac6d8eb423427e4eac4dc42d2d7
Nicholas Cook <nickcook530@gmail.com>
2019-10-08 22:08:38 -0400

    Mainly added ability to split out search string and compare each word against array synonyms for a given category

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
+// Should eventually move this to local storage
+var categories = {
+    Headwear: ["hat", "beanie", "cap"],
+    Footwear: ["socks", "sneakers", "boots"]
+};
+ 
 chrome.runtime.onInstalled.addListener(function(details){
     if(details.reason == "install"){
         //call a function to handle a first install
@@ -9,6 +15,7 @@ chrome.runtime.onInstalled.addListener(function(details){
 chrome.runtime.onStartup.addListener(function(){
     //Might be able to replace this with onInstalled call
     console.log("this is the startup call from background.js");
+    chrome.browserAction.setBadgeText({text: ''}); //remove badge that denoted previous results
 
     chrome.storage.local.get(function(result){
         if (typeof result.word_synonyms === 'undefined') {
@@ -23,17 +30,35 @@ chrome.runtime.onStartup.addListener(function(){
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
+        var api_category; // initiate to set later for use in api call
         if (request.msg === "content_completed") {
             //  To do something
-            let category = request.data.search_term;
-            console.log(category);
+            let search_term_array = request.data.search_term_array;
+            console.log("Array of searched words: ")
+            console.log(search_term_array);
 
-
-
+            search_term_array.forEach(function(search_word) {
+                if (api_category === undefined) {//Use to break loop at first instance of a match. Might need a better solution long term
+                    for (var category in categories) {
+                        if (!categories.hasOwnProperty(category)) {
+                            //The current property is not a direct property of object
+                            continue;
+                        }
+                        //Do your logic with the property here
+                        if (categories[category].includes(search_word)) {
+                            api_category = category;
+                            console.log("hit break with word: "+search_word);
+                            break;
+                        }
+                    }
+                } else {
+                    console.log("skipped "+search_word+" because api_category is already defined");
+                }
+            });
 
             var xhr = new XMLHttpRequest();
             xhr.responseType = 'json';
-            xhr.open('GET', "http://givebackpact.herokuapp.com/api/categories/" + category + "/companies");
+            xhr.open('GET', "http://givebackpact.herokuapp.com/api/categories/" + api_category + "/companies");
             xhr.onload = function() {
                 if (xhr.status === 200) {
                     var result_array = xhr.response;
--- content.js
+++ content.js
@@ -1,13 +1,16 @@
 console.log('content script fired')
 
 let search_term = document.getElementById("twotabsearchtextbox").value
+search_term = search_term.toLowerCase();
 console.log("User searched for: " + search_term)
+let search_term_array = search_term.split(" ");
+console.log(search_term_array);
 
 if (search_term){
     chrome.runtime.sendMessage({
         msg: "content_completed", 
         data: {
-            search_term: search_term
+            search_term_array: search_term_array
         }
     });
 }else {
