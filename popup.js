a69839b62603e6ecfa0aa494ed73bc25c8160ba4
Nicholas Cook <nickcook530@gmail.com>
2019-10-12 12:22:07 -0400

    Fixed console error with popup js if no results. Added temporary icon. Slight style changes to popup.

--- popup.js
+++ popup.js
@@ -8,16 +8,18 @@ window.onload = function() {
         console.log(companies);
         
         let parent = document.getElementById("company_list");
-        companies.forEach(company => {
-            //let company_div = document.createElement("div");
-            let company_li = document.createElement("li");
-            let hyperlink = document.createElement("a");
-            let node = document.createTextNode(company.name);
-            hyperlink.appendChild(node);
-            hyperlink.href = company.website_url
-            hyperlink.setAttribute('target', '_blank');
-            company_li.appendChild(hyperlink);
-            parent.appendChild(company_li);
-        });
+        if (typeof companies !== "undefined") {
+            companies.forEach(company => {
+                //let company_div = document.createElement("div");
+                let company_li = document.createElement("li");
+                let hyperlink = document.createElement("a");
+                let node = document.createTextNode(company.name);
+                hyperlink.appendChild(node);
+                hyperlink.href = company.website_url
+                hyperlink.setAttribute('target', '_blank');
+                company_li.appendChild(hyperlink);
+                parent.appendChild(company_li);
+            });
+        }
     });
 };
